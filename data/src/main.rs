9b1f6afe53c52fb87995e5c653d2d35149a74dde
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2023-09-12 09:53:10 +0700

    read file

--- data/src/main.rs
+++ data/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // Open the file
+    let mut file = match File::open("path/to/your/file.txt") {
+        Ok(file) => file,
+        Err(error) => panic!("Failed to open file: {}", error),
+    };
+
+    // Read the contents of the file into a string
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => {
+            println!("File contents:\n{}", contents);
+        }
+        Err(error) => {
+            panic!("Failed to read file: {}", error);
+        }
+    }
 }
